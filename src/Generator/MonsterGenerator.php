fe024ecc1f91d3f5f9181daff684938841a50013
UserKazun <developer.kazu0941@gmail.com>
2023-08-27 17:48:46 +0900

    update generator

--- src/Generator/MonsterGenerator.php
+++ src/Generator/MonsterGenerator.php
@@ -4,9 +4,20 @@ declare(strict_types=1);
 
 namespace MonsterQuest\Generator;
 
+use MonsterQuest\Dto\MonsterDto;
+
 class MonsterGenerator
 {
-    public function execute()
+    /**
+     * @param string $name
+     * @param int $level
+     * @param int $hp
+     * @param int $mp
+     * 
+     * @return MonsterDto
+     */
+    public function execute(string $name, int $level, int $hp, int $mp)
     {
+        return new MonsterDto(name: $name, level: $level, hp: $hp, mp: $mp);
     }
 }
